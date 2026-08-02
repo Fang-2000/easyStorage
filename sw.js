73a011494fb77abe0dd7d17791c7d26547d28d88
Fang-2000 <862237572@qq.com>
2026-08-02 20:31:12 +0800

    Fix blank Vercel page when Chart.js is unavailable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'my-items-v2';
+const CACHE = 'my-items-v3';
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./','./index.html','./css/styles.css','./js/app.js','./manifest.json','./icons/icon.svg']))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put(event.request,copy)); return response; }).catch(() => caches.match('./index.html')))));
